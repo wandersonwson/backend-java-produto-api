dbbfd1555ff61e6e4a4e9e4fae96b1c2eb6d6a2f
wandersonwson <wandersonwson@hotmail.com>
2024-06-19 22:38:16 -0300

    Início

---
+++ src/main/java/dev/wson/produto_api/ProdutoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.wson.produto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wson/produto_api/ProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wson.produto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

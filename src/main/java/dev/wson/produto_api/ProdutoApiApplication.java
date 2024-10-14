a6bdfac0a715ec6a5a2f29219818fbcdff7040ca
wandersonwson <wandersonwson@hotmail.com>
2024-10-13 22:57:42 -0300

    Correção do controlador de produtos

--- src/main/java/dev/wson/produto_api/ProdutoApiApplication.java
+++ src/main/java/dev/wson/produto_api/ProdutoApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProdutoApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutoApiApplication.class, args);
 	}
-
 }
--- src/main/java/dev/wson/produto_api/controladores/ProdutoController.java
+++ src/main/java/dev/wson/produto_api/controladores/ProdutoController.java
@@ -2,39 +2,34 @@ package dev.wson.produto_api.controladores;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import dev.wson.produto_api.modelos.ProdutoDTO;
 import dev.wson.produto_api.servicos.ProdutoService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/produto")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
-    @PostMapping("/produto")
+    @PostMapping
     public ProdutoDTO salvar(@Valid @RequestBody ProdutoDTO produto) {
         return produtoService.salvar(produto);
     }
-    @GetMapping("/produto")
+    @GetMapping
     public List<ProdutoDTO> buscarTodos() {
         return produtoService.buscarTodos();
     }
-    @GetMapping("/produto")
+    @GetMapping("/{categoriaId}")
     public List<ProdutoDTO> buscarPorCategoria(@RequestParam(name = "categoriaId", required = true) long categoriaId) {
         return produtoService.buscarPorCategoria(categoriaId);
     }
-    @GetMapping("/produto/{produtoId}")
+    @GetMapping("/{produtoId}")
     public ProdutoDTO buscarPorProdutoId(@PathVariable String produtoId) {
         return produtoService.buscarPorProdutoId(produtoId);
     }
-    @DeleteMapping("/usuario/{id}")
+    @DeleteMapping("/{id}")
     public void excluir(@PathVariable long id) {
         produtoService.excluir(id);
     }
